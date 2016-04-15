8d2be1e914b4d2f7d16a6200eb22965c2843f4cb
fdmerle <fdmerle@gmail.com>
2016-04-16 04:12:25 +0800

    secondDraft

--- src/main/java/pages/GmailPage.java
+++ src/main/java/pages/GmailPage.java
@@ -8,6 +8,7 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.open;
 
+
 import org.openqa.selenium.NoSuchElementException;
 
 
